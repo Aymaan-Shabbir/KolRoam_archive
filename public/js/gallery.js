72004240ab71b2dddfcf6fd5aee23ec7e7db5a08
Aymaan-Shabbir <aymaanshabbir27@gmail.com>
2023-10-29 00:39:07 +0530

    minor fixex

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,44 +1,43 @@
-var menuToggle = document.getElementById('menu-toggle');
-var nleft = document.getElementById('nleft');
+var menuToggle = document.getElementById("menu-toggle");
+var nleft = document.getElementById("nleft");
 // var nright = document.getElementById('nright');
-var nav = document.getElementById('nav');
+var nav = document.getElementById("nav");
 
-menuToggle.addEventListener('click', function() {
-  nleft.style.display = (nleft.style.display === 'none' || nleft.style.display === '') ? 'flex' : 'none';
+menuToggle.addEventListener("click", function () {
+  nleft.style.display =
+    nleft.style.display === "none" || nleft.style.display === ""
+      ? "flex"
+      : "none";
   // nright.style.display = (nright.style.display === 'none' || nright.style.display === '') ? 'flex' : 'none';
 
   // If the menu is open, set flex direction to column for nleft and nright
-  if (nleft.style.display === 'flex') {
-    nleft.style.flexDirection = 'column';
-    nleft.style.marginTop = '1rem';
+  if (nleft.style.display === "flex") {
+    nleft.style.flexDirection = "column";
+    nleft.style.marginTop = "1rem";
     // nright.style.flexDirection = 'column';
-    nav.style.background = '#004141';
-    menuToggle.style.marginRight = '1rem';
+    nav.style.background = "#004141";
+    menuToggle.style.marginRight = "1rem";
   } else {
     // If the menu is closed, reset flex direction to row for nleft and nright
-    nleft.style.flexDirection = 'row';
+    nleft.style.flexDirection = "row";
     // nright.style.flexDirection = 'row';
-    nleft.style.marginTop = '0';
-    nav.style.background = 'transparent';
-    menuToggle.style.marginRight = '0';
-
+    nleft.style.marginTop = "0";
+    nav.style.background = "transparent";
+    menuToggle.style.marginRight = "0";
   }
 });
 
 // Update the visibility when resizing the window
-window.addEventListener('resize', function() {
-  if (window.innerWidth > 582) {
-    nleft.style.display = 'flex';
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 600) {
+    nleft.style.display = "flex";
     // nright.style.display = 'flex';
-    
   } else {
-    nleft.style.display = 'none';
+    nleft.style.display = "none";
     // nright.style.display = 'none';
-    menuToggle.style.marginRight = '0';
-   
+    menuToggle.style.marginRight = "0";
   }
 });
-
 
 AOS.init();
 
@@ -46,12 +45,11 @@ function refreshAOS() {
   AOS.refresh();
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Restore like counts from local storage on page load
-  const cards = document.querySelectorAll('.card');
-  cards.forEach(card => {
-    const likeKey = card.querySelector('.like-count').getAttribute('id');
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    const likeKey = card.querySelector(".like-count").getAttribute("id");
     const likeCountElement = document.getElementById(likeKey);
     const storedLikes = localStorage.getItem(likeKey);
     if (storedLikes !== null) {
@@ -68,14 +66,14 @@ function toggleLike(imageContainer, likeCountId, starPopupId) {
   likeCountElement.textContent = currentLikes + 1;
 
   // Store the updated like count in local storage
-  const likeKey = likeCountElement.getAttribute('id');
+  const likeKey = likeCountElement.getAttribute("id");
   localStorage.setItem(likeKey, likeCountElement.textContent);
 
-  starPopup.style.display = 'block';
+  starPopup.style.display = "block";
 
   // Remove star popup after 2 seconds
   setTimeout(() => {
-    starPopup.style.display = 'none';
+    starPopup.style.display = "none";
   }, 2000);
 }
 
@@ -87,16 +85,13 @@ function increaseLikes(likeCountId, starPopupId) {
   likeCountElement.textContent = currentLikes + 1;
 
   // Store the updated like count in local storage
-  const likeKey = likeCountElement.getAttribute('id');
+  const likeKey = likeCountElement.getAttribute("id");
   localStorage.setItem(likeKey, likeCountElement.textContent);
 
-  starPopup.style.display = 'block';
+  starPopup.style.display = "block";
 
   // Remove star popup after 2 seconds
   setTimeout(() => {
-    starPopup.style.display = 'none';
+    starPopup.style.display = "none";
   }, 2000);
 }
-
-     
-
