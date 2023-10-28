379e3d9301ccef54546e7b31bad517fb39039ec2
Aymaan-Shabbir <aymaanshabbir27@gmail.com>
2023-10-29 02:57:30 +0530

    new updates

--- public/js/home.js
+++ public/js/home.js
@@ -1,59 +1,35 @@
-Shery.imageEffect(".back", {
-  style: 5,
-  config: {
-    a: { value: 2, range: [0, 30] },
-    b: { value: 0.75, range: [-1, 1] },
-    aspect: { value: 2.16284395530463 },
-    gooey: { value: true },
-    infiniteGooey: { value: true },
-    durationOut: { value: 1, range: [0.1, 5] },
-    durationIn: { value: 1.5, range: [0.1, 5] },
-    displaceAmount: { value: 0.5 },
-    masker: { value: true },
-    maskVal: { value: 1, range: [1, 5] },
-    scrollType: { value: 0 },
-    geoVertex: { range: [1, 64], value: 1 },
-    noEffectGooey: { value: false },
-    onMouse: { value: 1 },
-    noise_speed: { value: 0.2, range: [0, 10] },
-    metaball: { value: 0.2, range: [0, 2] },
-    discard_threshold: { value: 0.5, range: [0, 1] },
-    antialias_threshold: { value: 0, range: [0, 0.1] },
-    noise_height: { value: 0.5, range: [0, 2] },
-    noise_scale: { value: 10, range: [0, 100] },
-  },
-  gooey: true,
-});
+// set index and transition delay
+let index = 0;
+let transitionDelay = 3000;
 
-var elems = document.querySelectorAll(".elem");
+// get div containing the slides
+let slideContainer = document.querySelector(".slideshow");
+// get the slides
+let slides = slideContainer.querySelectorAll(".slide");
 
-elems.forEach(function (elem) {
-  var h1s = elem.querySelectorAll("h1");
-  var index = 0;
-  var animating = false;
-  document.querySelector(".main").addEventListener("click", function () {
-    if (!animating) {
-      animating = true;
-      gsap.to(h1s[index], {
-        top: "-=100%",
-        ease: Expo.easeInOut,
-        duration: 0.5,
-        onComplete: function () {
-          gsap.set(this._targets[0], { top: "100%" });
-          // btn.style.pointerEvents = all;
-          animating = false;
-        },
-      });
-      index === h1s.length - 1 ? (index = 0) : index++;
+// set transition delay for slides
+for (let slide of slides) {
+  slide.style.transition = `all ${transitionDelay / 1000}s linear`;
+}
 
-      gsap.to(h1s[index], {
-        top: "-=100%",
-        ease: Expo.easeInOut,
-        duration: 0.5,
-      });
-    }
+// show the first slide
+showSlide(index);
+
+// show a specific slide
+function showSlide(slideNumber) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i == slideNumber ? "block" : "none";
   });
-});
+  // next index
+  index++;
+  // go back to 0 if at the end of slides
+  if (index >= slides.length) {
+    index = 0;
+  }
+}
+
+// transition to next slide every x seconds
+setInterval(() => showSlide(index), transitionDelay);
 
 var menuToggle = document.getElementById("menu-toggle");
 var nleft = document.getElementById("nleft");
